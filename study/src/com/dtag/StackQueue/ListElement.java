2e3cede5ac0db2232616d70da516fa427cf693df
Nezbudii Aleksei <Aleksei.Nezbudii@t-systems.ru>
2020-11-17 16:17:47 +0300

    Initial commit

---
+++ study/src/com/dtag/StackQueue/ListElement.java
@@ -0,0 +1,28 @@
+package com.dtag.StackQueue;
+
+public class ListElement {
+
+    private ListElement next;
+    private String value;
+
+    public ListElement(ListElement next, String value) {
+        setNext(next);
+        setValue(value);
+    }
+
+    public ListElement getNext() {
+        return next;
+    }
+
+    public void setNext(ListElement next) {
+        this.next = next;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
